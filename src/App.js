de6d810a27aba784559452fb4f3549c1b04a2274
YoonhoKim655 <maumnamu2@naver.com>
2021-04-13 11:09:25 +0900

    1step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) //props.fav = {fav} 
+{
+  return (
+  <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
+  );
+}
+
+const foodILike = [
+  {
+    name : "kimchi",
+    image : "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.imweb.me%2Fthumbnail%2F20200415%2F6b6e035658bac.png&imgrefurl=https%3A%2F%2Fwww.ffd.co.kr%2Fshop-constant%2F%3Fidx%3D41&tbnid=uDdIaJQ0fdYYvM&vet=12ahUKEwiyprWYj_rvAhUKUJQKHXRrBLcQMygAegUIARDZAQ..i&docid=qjF76-128-DOTM&w=750&h=750&q=%EA%B9%80%EC%B9%98&ved=2ahUKEwiyprWYj_rvAhUKUJQKHXRrBLcQMygAegUIARDZAQ"
+  },
+  {
+    name : "samgyopsal",
+    image : "https://www.google.com/imgres?imgurl=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F222d88e5c7dc496c8e8a8a56c3452e52.JPG&imgrefurl=https%3A%2F%2F1boon.kakao.com%2Fdailylife%2F181214_1&tbnid=uiCwBwFCULxa2M&vet=12ahUKEwjipMnGj_rvAhWISJQKHR0QDigQMygJegUIARDQAQ..i&docid=xEJro_sWJLFxRM&w=600&h=600&q=%EC%82%BC%EA%B2%B9%EC%82%B4&ved=2ahUKEwjipMnGj_rvAhWISJQKHR0QDigQMygJegUIARDQAQ"
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    {foodILike.map(dish => (
+    <Food name = {dish.name} picture = {dish.image}/>
+    ))}
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
+/*app .js에 있는 App() function을 반환
+react는 한번에 하나의 component만을 반환(render),
+따라서 모든 것은 application안에 들어가야만한다.
+많은 component들을 하나의 app에 넣어야한다.
+*/
