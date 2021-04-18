9c4d9d6e229743b75066ecf4b71e9b32e04bddc8
YoonhoKim655 <maumnamu2@naver.com>
2021-04-18 15:34:08 +0900

    Planinng the movie component

--- src/App.js
+++ src/App.js
@@ -2,46 +2,24 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component
-{
-  constructor(props){//component가 web으로 갈 때 호출되는 method
-    super(props);
-    console.log("Hello");
-  }//render보다 먼저 실행
+{ 
   state = {
-    count : 0
-  }//it can be change
-  add = () => {
-    this.setState(current => ({count : current.count + 1}));
+    isLoading : true,
+    movies : []
   };
-  minus = () => {
-    this.setState({count : this.state.count - 1});
-  };
-  //state를 직접적으로 바꾸면 오류
-
+  
   componentDidMount()
   {
-   console.log("component rendered"); 
-  }
-
-  componentDidUpdate()//state가 바뀌는 것과 같이 어떤 것이 업데이트 되면 발생
-  {
-    console.log("i just updated");
-  }
-
-  componentWillUnmount()//component가 떠날 때 동작
-  {
-    console.log("Goodbye, cruel world");
-  }
+    setTimeout(() => 
+    {
+      this.setState({isLoading : false});
+    }, 6000);//파라미터로 선언되지 않은 변수를 입력 받을 수 있다. 미래에 사용하는 것을 먼저 쓰는 습관은 좋은 것이다.
+  }/*componentDidMount는 render후 바로 실행이되는 method이므로 
+     쉽게 render후에 state를 조절할 수 있다.*/
   render()
   {
-    console.log("render");
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick = {this.add}>Add</button>
-        <button onClick = {this.minus}>Minus</button>
-      </div>
-    );
+    const {isLoading} = this.state;
+      return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
-}//1. constructor  2. render  3. componentDidMount의 순서로 실행
+}
 export default App;
