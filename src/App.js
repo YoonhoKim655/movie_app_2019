afe686896a2ea746e5f6971f577e62a5ab359d9c
YoonhoKim655 <maumnamu2@naver.com>
2021-04-19 14:51:01 +0900

    Rendering Movie Data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component
 { 
@@ -7,19 +8,28 @@ class App extends React.Component
     isLoading : true,
     movies : []
   };
-  
+  getMovies = async () => { // async는 비동기 함수로 기다리라는 의미
+    const {data: {data : {movies}}} =  await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading : false}); //movie state에 axios의 movie 삽입
+  } //axios는 javascript의 fetch와 같이 크롤링하는 기능(npm i axios)
+//await는 async에서 기다리는 것의 주체 => axios가 끝날 때까지 기다리는 것
   componentDidMount()
   {
-    setTimeout(() => 
-    {
-      this.setState({isLoading : false});
-    }, 6000);//파라미터로 선언되지 않은 변수를 입력 받을 수 있다. 미래에 사용하는 것을 먼저 쓰는 습관은 좋은 것이다.
+    this.getMovies();
   }/*componentDidMount는 render후 바로 실행이되는 method이므로 
      쉽게 render후에 state를 조절할 수 있다.*/
   render()
   {
-    const {isLoading} = this.state;
-      return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    const {isLoading, movies} = this.state;
+      return (<div>{isLoading ? "Loading" : movies.map(movie =>(
+      <Movie
+        key={movie.id}
+        id = {movie.id} 
+        year = {movie.year}
+        title = {movie.title}
+        summary= {movie.summary} 
+        poster= {movie.medium_cover_image} />
+        ))}</div>);
   }
 }
 export default App;
