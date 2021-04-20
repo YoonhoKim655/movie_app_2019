3f7d527bf4e0f1b030bc8c0a6e0e5fe96e1673a6
YoonhoKim655 <maumnamu2@naver.com>
2021-04-20 16:39:25 +0900

    Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Proptypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster})
+function Movie({year, title, summary, poster})
 {
-    return <h4>{title}</h4>
-}
+    return <div class = "movie">
+        <img src = {poster} alt={title} title = {title}/> 
+        <div class="movie_class">
+            <h3 class = "movie_title">{title}</h3>
+            <h5 class = "movie_year">{year}</h5>
+            <p class ="movie_summary">{summary}</p>
+        </div>
+    </div>;
+}//alt는 포스터에 커서를 놓았을 때 표시되는 텍스트
 
 Movie.propTypes = {
     id : Proptypes.number.isRequired,
