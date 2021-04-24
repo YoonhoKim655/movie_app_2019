e3f72426e91d666f2c909127b895613df1031075
YoonhoKim655 <maumnamu2@naver.com>
2021-04-24 22:51:46 +0900

    Adding Genres

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Proptypes from "prop-types";
 import "./Movie.css";
 
-function Movie({year, title, summary, poster})
+function Movie({year, title, summary, poster, genres})
 {
-    return <div class = "movie">
+    return <div className = "movie">
         <img src = {poster} alt={title} title = {title}/> 
-        <div class="movie_class">
-            <h3 class = "movie_title">{title}</h3>
-            <h5 class = "movie_year">{year}</h5>
-            <p class ="movie_summary">{summary}</p>
+        <div className="movie_class">
+            <h3 className = "movie_title">{title}</h3>
+            <h5 className = "movie_year">{year}</h5>
+            <ul className="genres">
+                {genres.map((genre, index) => (
+                    <li key = {index} className="genres_genre">{genre}</li>
+                ))}
+            </ul>
+            <p className ="movie_summary">{summary}</p>
+            
         </div>
     </div>;
 }//alt는 포스터에 커서를 놓았을 때 표시되는 텍스트
@@ -19,7 +25,8 @@ Movie.propTypes = {
     year : Proptypes.number.isRequired,
     title : Proptypes.string.isRequired,
     summary: Proptypes.string.isRequired,
-    poster: Proptypes.string.isRequired
+    poster: Proptypes.string.isRequired,
+    genres: Proptypes.arrayOf(Proptypes.string).isRequired
 };
 
 
